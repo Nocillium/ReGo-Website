b3be5b22788337d3eaebb3a2e162ba1feb77f593
Chester Cruz <chester@clavionlabs.com>
2026-08-10 16:18:00 +0800

    Update site contact details

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 
   const SITE_CONFIG = window.REGO_SITE_CONFIG || {};
   const CONFIG = {
-    phone: '+15551234567',
+    phone: '+12709841303',
     shopmonkeyUrl: 'https://www.shopmonkey.io/book/your-shop-id',
     shopmonkeyApiUrl: '/api/appointment',
     googleMapsEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.966471254!2d-74.00425878459418!3d40.74076697932881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQ0JzI2LjgiTiA3NMKwMDAnMTUuMyJX!5e0!3m2!1sen!2sus!4v1234567890',
